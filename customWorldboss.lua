d4bf8f0228096aff1df1c0cf3c22fc35c0687327
55Honey <71938210+55Honey@users.noreply.github.com>
2021-05-20 13:10:17 +0200

    Update customWorldboss.lua

--- customWorldboss.lua
+++ customWorldboss.lua
@@ -13,6 +13,7 @@
 -- ADMIN GUIDE:  -  compile the core with ElunaLua module
 --               -  adjust config in this file
 --               -  add this script to ../lua_scripts/
+--               -  run .initcustomworldboss to add the required NPCs
 ------------------------------------------------------------------------------------------------
 -- GM GUIDE:     -  use .startevent $event $difficulty to start and spawn 
 --               -  maybe offer teleports               '
@@ -25,14 +26,14 @@ local Config_addAmount = {}
 local Config.customDbName = "ac_eluna"
 -- Min GM rank to start an event
 local Config.GMRankForEventStart = 2
+-- Min GM rank to add NPCs to the db
+local Config.GMRankForUpdateDB = 3
 -- set to 1 to print error messages to the console. Any other value including nil turns it off.
 local Config.printErrorsToConsole = 1 
 
 -- NPC to spawn for event [n]
 Config_npcEntry[1] = 1112001
 Config_addAmount[1] = 3
-local eventNPC
-local difficulty
 
 ------------------------------------------
 -- NO ADJUSTMENTS REQUIRED BELOW THIS LINE
@@ -43,6 +44,10 @@ local TEMPSUMMON_DEAD_DESPAWN = 7        -- despawns when the creature disappear
 local TEMPSUMMON_MANUAL_DESPAWN = 8      -- despawns when UnSummon() is called
 local GOSSIP_EVENT_ON_SELECT = 2         -- (event, player, object, sender, intid, code, menu_id)
 
+local eventNPC
+local difficulty
+local eventInProgress
+local cancelGossipEvent
 -- todo: make a function to add the custom NPC to creature_template
 
 local function eS_command(event, player, command)
@@ -67,21 +72,52 @@ local function eS_command(event, player, command)
   
     if commandArray[2] == nil then commandArray[2] = 1 end
     if commandArray[3] == nil then commandArray[3] = 1 end
-    
+  
     if commandArray[1] == "startevent" then
         eventNPC = tonumber(commandArray[2])
         difficulty = tonumber(commandArray[3])
         summonEventNPC()
         return false
     end
+    
+    --prevent non-Admins from using the rest
+    if player ~= nil then  
+        if player:GetGMRank() < Config.GMRankForUpdateDB then
+            return
+        end  
+    end
+    
+    if commandArray[] == "initcustomworldboss" then
+        initCustomWorldBoss()
+    end
 end
     
 local function summonEventNPC()
+    -- tempSummon an NPC with a dialouge option to start the encounter
     print("summonEventNPC")
+    -- add an event to spawn the Boss in a phase when the only gossip is clicked
+    cancelGossipEvent = RegisterCreatureGossipEvent(Config_npcEntry[n], GOSSIP_EVENT_ON_SELECT, spawnBoss)   
 end
 
-local function spawnBoss(NPC, difficulty)
+local function initCustomWorldBoss()
+    -- query to add the costom NPCs to the db
+    print("initCustomWorldBoss")
+end
+
+local function spawnBoss(event, player, object, sender, intid, code, menu_id)
     print("spawnBoss")
+    --spawn the boss in another phase
+    --move the whole raid to the boss-phase
+end
+
+local function getSize(difficulty)
+    local value
+    if diffculty == 1 then
+        value = 1
+    else
+        value = (difficulty - 1) / 4
+    end
+    return value
 end
 
 local function eS_splitString(inputstr, seperator)
@@ -96,6 +132,5 @@ local function eS_splitString(inputstr, seperator)
 end
 
 RegisterPlayerEvent(PLAYER_EVENT_ON_COMMAND, eS_command)
-local cancelGossipEvent = RegisterCreatureGossipEvent(Config_npcEntry[n], GOSSIP_EVENT_ON_SELECT, spawnBoss) 
---todo: Try this! maybe it's (event, player, object, sender, intid, code, menu_id)
+
 --todo: Try creating a gossip
