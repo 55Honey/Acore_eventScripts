1ad9cb4daef4a1137ae5e47386c0146d013c1d31
55Honey <71938210+55Honey@users.noreply.github.com>
2021-06-28 10:49:26 +0200

    adjust down sql for prior commit

--- database/world/customWorldboss-down.sql
+++ database/world/customWorldboss-down.sql
@@ -1,4 +1,4 @@
-DELETE FROM `creature_template` WHERE `entry` IN (1112001, 1112002, 1112003, 1112011, 1112013);
-DELETE FROM `npc_text` WHERE `ID` = 91111;
-DELETE FROM `gossip_menu` WHERE `MenuID` = 62001;
+DELETE FROM `creature_template` WHERE `entry` IN (1112001,1112002,1112003,1112011,1112012,1112013);
+DELETE FROM `npc_text` WHERE `ID` IN (91111,91112);
+DELETE FROM `gossip_menu` WHERE `MenuID` IN (62001,62001);
 DELETE FROM `creature_equip_template` WHERE `CreatureID` = '1112011';
