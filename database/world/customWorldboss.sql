d53129bd3542f58d08e9c638700d987737ed28e2
55Honey <71938210+55Honey@users.noreply.github.com>
2021-06-29 10:33:10 +0200

    chore: fix typo

--- database/world/customWorldboss.sql
+++ database/world/customWorldboss.sql
@@ -1,6 +1,6 @@
 DELETE FROM `creature_template` WHERE `entry` IN (1112001,1112002,1112003,1112011,1112012,1112013);
 DELETE FROM `npc_text` WHERE `ID` IN (91111,91112);
-DELETE FROM `gossip_menu` WHERE `MenuID` IN (62001,62001);
+DELETE FROM `gossip_menu` WHERE `MenuID` IN (62001,62002);
 DELETE FROM `creature_equip_template` WHERE `CreatureID` = '1112011';
 
 INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `InhabitType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
