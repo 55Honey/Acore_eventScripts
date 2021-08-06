6d6a23b86e333f660d2a7122f18ea6579a8275a4
Stefano Borzì <stefanoborzi32@gmail.com>
2021-08-06 19:37:45 +0200

    Update query-points.sql

--- scripts/query-points.sql
+++ scripts/query-points.sql
@@ -4,4 +4,6 @@ SELECT CONCAT(
     `ac_eluna`.`eventscript_score`.`score_earned_current`,
     ' WHERE meta_key = "mycred_default" AND user_id =',
     account_id, ';') AS `query` FROM ac_eluna.eventscript_score;
-
+    
+-- run this ONLY if you already distributed the points
+UPDATE ac_eluna.eventscript_score SET `score_earned_current` = 0;
