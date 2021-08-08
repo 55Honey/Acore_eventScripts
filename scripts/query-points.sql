f94dd02c30410013dbc8a12b6d09144e02205865
Stefano Borzì <stefanoborzi32@gmail.com>
2021-08-08 19:40:35 +0200

    chore: update sql scripts

--- scripts/query-points.sql
+++ scripts/query-points.sql
@@ -1,6 +1,6 @@
 -- compatible with acore-cms and mycred plugin
 SELECT CONCAT(
-    'UPDATE wp_usermeta SET meta_value = meta_value + ',
+    'UPDATE wp_usermeta SET meta_value = CAST(`meta_value` AS UNSIGNED) + ',
     `ac_eluna`.`eventscript_score`.`score_earned_current`,
     ' WHERE meta_key = "mycred_default" AND user_id =',
     account_id, ';') AS `query` FROM ac_eluna.eventscript_score;
