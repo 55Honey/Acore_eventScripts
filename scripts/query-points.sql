94844b2b54c0a7bc5f973bc25ad29d77a1feb34d
Stefano Borzì <stefanoborzi32@gmail.com>
2021-08-08 20:43:18 +0200

    fix: sql scripts

--- scripts/query-points.sql
+++ scripts/query-points.sql
@@ -2,9 +2,9 @@
 SELECT CONCAT(
     'UPDATE wp_usermeta SET meta_value = CAST(`meta_value` AS UNSIGNED) + ',
     `ac_eluna`.`eventscript_score`.`score_earned_current`,
-    ' WHERE meta_key = "mycred_default" AND user_id =',
-    account_id, ';') AS `query` FROM ac_eluna.eventscript_score;
-    
+    ' WHERE meta_key = "mycred_default" AND user_id = (SELECT ID FROM wp_users WHERE user_login = ',
+    (SELECT username FROM acore_auth.account WHERE id=account_id), ');') AS `query` FROM ac_eluna.eventscript_score;
+
 -- run this ONLY if you already distributed the points
 UPDATE ac_eluna.eventscript_score SET `score_earned_current` = 0;
 
